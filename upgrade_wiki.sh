d6eca041f75d30dd6a7b44ee84a2253f14783a8b
Administrator <chris.wyse@wysechoice.net>
2023-02-06 14:04:12 +0000

    docs: upload upgrade_wiki.sh

--- upgrade_wiki.sh
+++ upgrade_wiki.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 NEW_VERSION=2.5.296
-OLD_VERSION=2.5.286 
+OLD_VERSION=2.5.296 
+
+# function to get IP address
+function get_ipaddr {
+  local hostname=${1}
+  local dnsserver=${2}
+  local query_type=${3} # A or AAAA (IPv6)
+  local ip_address=""
+
+  # A and AAA record for IPv4 and IPv6, respectively
+  # $1 stands for first argument
+  if [ -n "${hostname}" ]; then
+    if [ -z "${query_type}" ]; then
+      query_type="A"
+    fi
+    # use host command for DNS lookup operations
+    host -t ${query_type}  ${hostname} ${dnsserver} &>/dev/null 
+    if [ "$?" -eq "0" ]; then
+      # get ip address
+      ip_address="$(host -t ${query_type} ${hostname} ${dnsserver}| awk '/has.*address/{print $NF; exit}')"
+    else
+      return 1
+    fi
+  else
+    return 2
+  fi
+# display ip
+ echo $ip_address
+}
+
+hostname="wiki.wysechoice.net"
+dnsname="pihole.wysechoice.net"
+STATIC_IP=$(get_ipaddr $hostname $dnsname)
+DNS_SERVER_IP=$(get_ipaddr $dnsname $dnsname)
+
 # Stop and remove container named "wiki-<old_version>"
 docker stop wiki-${OLD_VERSION}
-#docker rm wiki-${NEW_VERSION}
+docker rm wiki-${OLD_VERSION}
 
 # Pull latest image of Wiki.js
 docker pull requarks/wiki:2
 
 # Create new container of Wiki.js based on latest image
 docker run -d                                                  \
-           --ip="192.168.40.33"                                \
-           --dns="192.168.5.3"                                 \
+           --ip="${STATIC_IP}"                                 \
+           --dns="${DNS_SERVER_IP}"                            \
            --mac-address="02:42:c0:a8:28:21"                   \
            -p 3000:3000                                        \
            --net=dockernet.40                                  \
@@ -24,10 +58,9 @@ docker run -d                                                  \
            -e "DB_USER=wikijs"                                 \
            -e "DB_PASS=wikijsrocks"                            \
            -e "DB_NAME=wiki"                                   \
-           -v wiki_data:/home/pi/docker_vols/wiki_data         \
-           -v wiki_content:/wiki/data/content                  \
+           -v wiki_data:/wiki/data/content                     \
            requarks/wiki:2
 
 # Use a second CPU for the container
-docker update --cpus 2 wiki-${NEW_VERSION}
+docker update --cpus 3 wiki-${NEW_VERSION}
 
